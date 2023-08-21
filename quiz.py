345b4d95fef96b45ca7d8156c27eb34a0358658e
Norbert Galazka <norbert1996galazka@gmail.com>
2023-08-21 11:27:38 +0200

    deleted unnecessary spaces

--- quiz.py
+++ quiz.py
@@ -10,14 +10,14 @@ class QuestionWithAnswer:
         self.result = result
         self.questions = []
         self.answers = []
-        self.num_of_qestions = num_of_questions
+        self.num_of_questions = num_of_questions
         self.sentences = sentences
 
     def __del__(self):
         print(self.sentences['del'])
 
-    def append_questions_and_ansers(self):
-        for i in range(self.num_of_qestions):
+    def append_questions_and_anwsers(self):
+        for i in range(self.num_of_questions):
             question = html.unescape(self.result['results'][i]['question'])
             translator = Translator(from_lang='en', to_lang=self.language)
             translation = translator.translate(question)
@@ -33,7 +33,7 @@ class QuestionWithAnswer:
         points = 0
         while self.questions:
             print(self.questions.pop())
-            user_response = input(self.sentences['user_res'] + "[y/n]")
+            user_response = input(self.sentences['user_res'] + " [y/n]: ")
             if user_response.lower() == 'y':
                 user_response = True
             else:
@@ -46,31 +46,30 @@ class QuestionWithAnswer:
         print(self.sentences["points"], points)
 
 
-def choose_lanuage(languages):
+def choose_language(languages):
     print(languages)
     lang = input("Choose your language: ")
     return lang
 
 
-
 def main():
     languages = ['en', 'pl', 'cs', 'fr', 'de', 'hu', 'pt', 'ru', 'es', 'uk']
-    chosen_lang = choose_lanuage(languages)
+    chosen_lang = choose_language(languages)
     sentences = {"del": "The question and answer set has been removed!",
-                 "user_res": "What is the correct answer yes or no? Enter: ",
+                 "user_res": "What is the correct answer yes or no? Enter ",
                  "well_done": "Well done, you guessed it!",
                  "error": "Sorry, error!",
                  "points": "Number of points scored: "}
     for key, value in sentences.items():
         translator = Translator(from_lang='en', to_lang=chosen_lang)
         translation = translator.translate(value)
-        sentences.update({key : translation})
+        sentences.update({key: translation})
 
     response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
     result = json.loads(response.text)
     first_game = QuestionWithAnswer(10, chosen_lang, result, sentences)
 
-    first_game.append_questions_and_ansers()
+    first_game.append_questions_and_anwsers()
     first_game.display_questions()
 
 
